5ed810d2b03616d8bae798cc54e1df5241b537d4
Oleg <37769766+TheDearbear@users.noreply.github.com>
2024-03-03 15:14:17 +0300

    Port render pass and framebuffer implementations

---
+++ src/VulkanFramebuffer.cs
@@ -0,0 +1,76 @@
+﻿using Silk.NET.Vulkan;
+using Speed.Engine.Textures;
+using System;
+using System.Linq;
+
+using SpeedRenderPass = Speed.Viewer.Render.Backend.RenderPass;
+using SpeedFramebuffer = Speed.Viewer.Render.Backend.Framebuffer;
+
+using VkFramebuffer = Silk.NET.Vulkan.Framebuffer;
+
+namespace VulkanModule;
+
+internal sealed unsafe class VulkanFramebuffer : SpeedFramebuffer
+{
+    internal VkFramebuffer Framebuffer { get; set; }
+
+    readonly VulkanTexture[] _attachments;
+    readonly VulkanRenderPass _renderPass;
+    readonly Device _device;
+    readonly Vk _vk;
+
+    public VulkanFramebuffer(Vk vk, int width, int height, Texture[] attachments, SpeedRenderPass renderPass)
+        : base(width, height, attachments, renderPass)
+    {
+        _vk = vk;
+        _device = _vk.CurrentDevice!.Value;
+
+        if (renderPass is not VulkanRenderPass vkRenderPass)
+        {
+            throw new ArgumentException("Provided render pass belongs to different backend.", nameof(renderPass));
+        }
+
+        VulkanTexture[] vkAttachments = attachments.OfType<VulkanTexture>().ToArray();
+        if (vkAttachments.Length != attachments.Length)
+        {
+            throw new ArgumentException("Some of attachments belongs to different backends.", nameof(attachments));
+        }
+
+        _attachments = vkAttachments;
+        _renderPass = vkRenderPass;
+
+        ImageView[] views = new ImageView[_attachments.Length];
+        for (int i = 0; i < views.Length; i++)
+        {
+            var img = _attachments[i].Image;
+            if (!img.HasValue)
+            {
+                throw new ArgumentException($"One of framebuffer attachments not initialized! (Attachment {i + 1})", nameof(attachments));
+            }
+
+            views[i] = img.Value.Item2;
+        }
+
+        fixed (ImageView* pViews = views)
+        {
+            FramebufferCreateInfo createInfo = new()
+            {
+                SType = StructureType.FramebufferCreateInfo,
+                RenderPass = _renderPass.RenderPass,
+                AttachmentCount = unchecked((uint)_attachments.Length),
+                PAttachments = pViews,
+                Width = unchecked((uint)Width),
+                Height = unchecked((uint)Height),
+                Layers = 1
+            };
+
+            VulkanTools.Ensure(_vk.CreateFramebuffer(_device, in createInfo, null, out var framebuffer));
+            Framebuffer = framebuffer;
+        }
+    }
+
+    public override void Dispose()
+    {
+        _vk.DestroyFramebuffer(_device, Framebuffer, null);
+    }
+}
---
+++ src/VulkanRenderPass.cs
@@ -0,0 +1,181 @@
+﻿using Silk.NET.Vulkan;
+using Speed.Viewer.Render.Backend;
+using System;
+using System.Linq;
+using System.Numerics;
+
+using SpeedAttachmentDescription = Speed.Viewer.Render.Backend.AttachmentDescription;
+using SpeedCommandBuffer = Speed.Viewer.Render.Backend.CommandBuffer;
+using SpeedRenderPass = Speed.Viewer.Render.Backend.RenderPass;
+
+using VkAttachmentDescription = Silk.NET.Vulkan.AttachmentDescription;
+using VkRenderPass = Silk.NET.Vulkan.RenderPass;
+
+namespace VulkanModule;
+
+internal unsafe class VulkanRenderPass : SpeedRenderPass
+{
+    public override bool Started => _cmd is not null;
+
+    internal VkRenderPass RenderPass { get; }
+
+    VulkanCommandBuffer? _cmd;
+
+    readonly Device _device;
+    readonly Vk _vk;
+
+    public VulkanRenderPass(Vk vk, params SpeedAttachmentDescription[] descriptions)
+        : base(descriptions)
+    {
+        _vk = vk;
+        _device = _vk.CurrentDevice!.Value;
+
+        AttachmentReference reference = new(0, ImageLayout.ColorAttachmentOptimal);
+
+        SubpassDescription subpass = new(
+            SubpassDescriptionFlags.None,
+            PipelineBindPoint.Graphics,
+            0, null,
+            1, &reference, null, null,
+            0, null);
+
+        SubpassDependency dependency = new(
+            Vk.SubpassExternal, null,
+            PipelineStageFlags.ColorAttachmentOutputBit, PipelineStageFlags.ColorAttachmentOutputBit,
+            AccessFlags.ColorAttachmentReadBit | AccessFlags.ColorAttachmentWriteBit, null,
+            DependencyFlags.None);
+
+        fixed (VkAttachmentDescription* pAttachments = descriptions.Select(x => Convert(x)).ToArray())
+        {
+            RenderPassCreateInfo createInfo = new()
+            {
+                SType = StructureType.RenderPassCreateInfo,
+                AttachmentCount = unchecked((uint)descriptions.Length),
+                PAttachments = pAttachments,
+                SubpassCount = 1,
+                PSubpasses = &subpass,
+                DependencyCount = 1,
+                PDependencies = &dependency
+            };
+
+            VulkanTools.Ensure(_vk.CreateRenderPass(_device, in createInfo, null, out var renderPass));
+            RenderPass = renderPass;
+        }
+    }
+
+    public VulkanRenderPass(Vk vk, VkRenderPass renderPass, params SpeedAttachmentDescription[] descriptions)
+        : base(descriptions)
+    {
+        _vk = vk;
+        RenderPass = renderPass;
+        _device = _vk.CurrentDevice!.Value;
+    }
+
+    public override void Dispose()
+    {
+        _vk.DestroyRenderPass(_device, RenderPass, null);
+    }
+
+    public override void Begin(SpeedCommandBuffer buffer, params ValueTuple<Vector4, float, uint>[] clearValues)
+    {
+        if (_cmd is not null)
+        {
+            throw new InvalidOperationException("Render pass already started.");
+        }
+
+        if (buffer is not VulkanCommandBuffer vkCommandBuffer)
+        {
+            throw new ArgumentException("Not compatible command buffer.", nameof(buffer));
+        }
+
+        if (buffer.CurrentFramebuffer is not VulkanFramebuffer vkFramebuffer)
+        {
+            throw new ArgumentException("Framebuffer not set.", nameof(buffer));
+        }
+
+        if (vkFramebuffer.Attachments.Length != 0 &&
+            vkFramebuffer.Attachments[0] is VulkanTexture vkTexture &&
+            vkTexture.Image.HasValue)
+        {
+            vkTexture.TransitionLayout(vkCommandBuffer, TextureLayout.ColorAttachment,
+                new(ImageAspectFlags.ColorBit, 0, Vk.RemainingMipLevels, 0, Vk.RemainingArrayLayers));
+        }
+
+        RenderPassBeginInfo beginInfo = new()
+        {
+            SType = StructureType.RenderPassBeginInfo,
+            RenderPass = RenderPass,
+            Framebuffer = vkFramebuffer.Framebuffer,
+            RenderArea = new(new(), new(unchecked((uint)vkFramebuffer.Width), unchecked((uint)vkFramebuffer.Height)))
+        };
+
+        if (clearValues.Length > 0)
+        {
+            fixed (ClearValue* pValues = clearValues.Select(x => Convert(x)).ToArray())
+            {
+                beginInfo.ClearValueCount = unchecked((uint)clearValues.Length);
+                beginInfo.PClearValues = pValues;
+            }
+        }
+
+        _vk.CmdBeginRenderPass(vkCommandBuffer.CommandBuffer, in beginInfo, SubpassContents.Inline);
+        _cmd = vkCommandBuffer;
+    }
+
+    public override void End()
+    {
+        if (_cmd is not null)
+        {
+            _vk.CmdEndRenderPass(_cmd.CommandBuffer);
+
+            if (_cmd.CurrentFramebuffer is VulkanFramebuffer vkFramebuffer &&
+                vkFramebuffer.Attachments.Length != 0 &&
+                vkFramebuffer.Attachments[0] is VulkanTexture vkTexture)
+            {
+                vkTexture.Layout = TextureLayout.Present;
+                vkTexture.NativeLayout = ImageLayout.PresentSrcKhr;
+            }
+
+            _cmd = null;
+        }
+    }
+
+    static VkAttachmentDescription Convert(SpeedAttachmentDescription description)
+    {
+        return new()
+        {
+            Format = VulkanTools.Convert(description.Format),
+            Samples = MapSamples(description.SampleCount),
+            LoadOp = AttachmentLoadOp.DontCare,
+            StoreOp = AttachmentStoreOp.Store,
+            StencilLoadOp = AttachmentLoadOp.DontCare,
+            StencilStoreOp = AttachmentStoreOp.DontCare,
+            InitialLayout = ImageLayout.Undefined,
+            FinalLayout = ImageLayout.PresentSrcKhr
+        };
+    }
+
+    static ClearValue Convert(ValueTuple<Vector4, float, uint> clearValue)
+    {
+        return new()
+        {
+            Color = new(clearValue.Item1.X, clearValue.Item1.Y, clearValue.Item1.Z, clearValue.Item1.W),
+            DepthStencil = new(clearValue.Item2, clearValue.Item3)
+        };
+    }
+
+    static SampleCountFlags MapSamples(int sampleCount)
+    {
+        if (sampleCount > 0x7F)
+        {
+            throw new ArgumentException("Too many samples! Requested: " + sampleCount, nameof(sampleCount));
+        }
+
+        if (sampleCount < 0)
+        {
+            throw new ArgumentException("Requested negative number of samples.", nameof(sampleCount));
+        }
+
+        return (SampleCountFlags)sampleCount;
+    }
+}
